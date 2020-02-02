34a5512994f41c5046ff8074934920ddd7dd8a99
Wojtek Przerwa <czescwojtek@gmail.com>
2020-02-02 20:09:03 +0100

    favicon added

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import { withPrefix } from 'gatsby'
 
 const SEO = ({
   title,
@@ -41,7 +42,25 @@ const SEO = ({
         content: descr
       }
     ]}
-  />
+  >
+    <link
+      rel='apple-touch-icon'
+      sizes='180x180'
+      href={`${withPrefix('/')}img/apple-touch-icon.png`}
+    />
+    <link
+      rel='icon'
+      type='image/png'
+      href={`${withPrefix('/')}img/favicon-32x32.png`}
+      sizes='32x32'
+    />
+    <link
+      rel='icon'
+      type='image/png'
+      href={`${withPrefix('/')}img/favicon-16x16.png`}
+      sizes='16x16'
+    />
+  </Helmet>
 )
 
 SEO.propTypes = {
