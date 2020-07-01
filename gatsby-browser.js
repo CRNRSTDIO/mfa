8636b3b253b5292b876ca7c73e7ad69a42cfb762
Wojtek Przerwa <czescwojtek@gmail.com>
2020-07-01 17:56:12 +0200

    packages update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,16 +4,16 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
-import 'gridlex'
-import './src/fonts/index.css'
-import './src/components/layout.scss'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import "gridlex"
+import "./src/fonts/index.css"
+import "./src/components/layout.scss"
 
-export { default as wrapRootElement } from './src/state'
+export { default as wrapRootElement } from "./src/state"
 
-export const onClientEntry = () => {
-  if (typeof window.IntersectionObserver === 'undefined') {
-    import('intersection-observer')
-    console.log('IntersectionObserver polyfill loaded')
+export const onClientEntry = async () => {
+  if (typeof window.IntersectionObserver === "undefined") {
+    await import("intersection-observer")
+    console.log("IntersectionObserver polyfill loaded")
   }
 }
